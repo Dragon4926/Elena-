5206c1cf2b4a2b6a97db44ce930e1be5d0a59ff2
Debopriyo Das <70844598+Dragon4926@users.noreply.github.com>
2025-04-06 10:36:13 +0530

    core functionality updates

--- ormconfig.js
+++ ormconfig.js
@@ -1,13 +1,9 @@
 module.exports = {
-  type: 'better-sqlite3', // Change this if switching DB
-  database: 'persona_bot.db', // Change this if switching DB
   entities: ['src/models/*.js'],
   migrations: ['src/migrations/*.js'],
   cli: {
     entitiesDir: 'src/models',
     migrationsDir: 'src/migrations'
   },
-  synchronize: false,
-  logging: true, // Logs to console by default if logger is not specified
-  // logger: 'file' // Remove this for Vercel
+  synchronize: false
 };
