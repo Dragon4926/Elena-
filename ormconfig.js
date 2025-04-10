7d8492d1e77657d060435c73403736d9a329d5f0
deadeye.040104 <deadeye.040104@gmail.com>
2025-04-10 10:51:22 +0000

    removed unnecessary files

--- ormconfig.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  entities: ['src/models/*.js'],
-  migrations: ['src/migrations/*.js'],
-  cli: {
-    entitiesDir: 'src/models',
-    migrationsDir: 'src/migrations'
-  },
-  synchronize: false
-};
